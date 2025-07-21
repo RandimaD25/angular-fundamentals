f4da94b90f94d3c4a6e63ba27ad9990797cf3c23
RandimaD25 <randimamethminid525@gmail.com>
2025-07-21 20:07:17 +0530

    feat: angular components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [RouterOutlet, Home, Header],
+  template: `
+    <app-header />
+    <main>
+      <app-home />
+    </main>
+  `,
+  styles: [
+    `
+      main {
+        padding: 16px;
+      }
+    `,
+  ],
+  // templateUrl: './app.html',
+  // styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('first-angular-app');
